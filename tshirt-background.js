94d78ffa5c4df282d4ccb0b546fe045bb9cb3a04
Dwan <me@dwaan.com>
2015-08-04 14:16:12 +0700

    Parallax background proof of concept

--- tshirt-background.js
+++ tshirt-background.js
@@ -78,3 +78,60 @@ ready(document, function () {
 addEvent (window, "resize", function() {
 	responsiveBackground ();
 });
+
+// Parallax Background
+ready(document, function () {
+	var element = document.getElementById("doEvent"),
+		oldInnerHTML = element.innerHTML;
+
+	element.innerHTML = "<div></div>";
+	element.childNodes[0].innerHTML = oldInnerHTML;
+
+	for (var i = 0; i < element.style.length; i++) {
+		element.childNodes[0].style[element.style[i]] = element.style[element.style[i]];
+	}
+
+	element.style.overflow = "hidden";
+	element.style.position = "relative";
+
+	element.childNodes[0].style.position = "absolute";
+	element.childNodes[0].style.top = -32;
+	element.childNodes[0].style.left = -32;
+	element.childNodes[0].style.width = element.offsetWidth + 64;
+	element.childNodes[0].style.height = element.offsetHeight + 64;
+	element.childNodes[0].style.backgroundSize = "cover";
+	element.childNodes[0].style.backgroundPosition = "center center";
+	element.childNodes[0].style.transition = "left 0.128s linear, top 0.128s linear";
+
+	console.log (element.childNodes[0]);
+
+	if (window.DeviceOrientationEvent) {
+		// Listen for the deviceorientation event and handle the raw data
+		window.addEventListener('deviceorientation', function(eventData) {
+			// alpha is the compass direction the device is facing in degrees
+			var dir = Math.round(eventData.alpha);
+
+			if (dir > 180) {
+				// gamma is the left-to-right tilt in degrees, where right is positive
+				var tiltLR = eventData.gamma;
+				tiltLR = Math.round((tiltLR / 90) * 64);
+				if (tiltLR > 32) tiltLR = 32;
+				if (tiltLR < -32) tiltLR = -32;
+
+				// beta is the front-to-back tilt in degrees, where front is positive
+				var tiltFB = eventData.beta;
+				tiltFB = Math.round(((tiltFB - 45) / 90) * 32);
+				if (tiltFB > 32) tiltFB = 32;
+				if (tiltFB < -32) tiltFB = -32;
+
+				// call our orientation event handler
+				document.getElementById("sniff").innerHTML = tiltLR + ", " + tiltFB + ", " + dir;
+
+				element.childNodes[0].style.top =  0 - 32 - tiltFB;
+				element.childNodes[0].style.left = 0 - 32 - tiltLR;
+			}
+		}, false);
+	} else {
+		document.getElementById("sniff").innerHTML = "Not supported."
+	}
+});
